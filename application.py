3a3846b60479e2a6d1d233f1c467ddde52420ef5
jmrshll <33238796+jmrshll@users.noreply.github.com>
2022-10-22 17:06:46 -0700

    debug

--- application.py
+++ application.py
@@ -20,28 +20,37 @@ import json
 #         '&units=imperial&appid=75ee49c19c9fac22f81ffcac43b80552').json()
 
 # print the temperature
-def print_temp(w):
-    return '<p>The temperature is %s degrees Fahrenheit.</p>\n' % (w['main']['temp'])
+# def print_temp(w):
+#     return '<p>The temperature is %s degrees Fahrenheit.' + '</p>\n' % (w['main']['temp'])
+
 
 # this method is for tabulating together the temperatures
 # a kilometer to the North, South, East, and West from
 # the input of the latitudinal and longitudinal coordinates
-# --> PLACEHOLDER <--
-# def tabulate_temps(int pseudovariable){
-        
-# }
+#
+# tabulate_temps
+def tabulate_temps(a=None, b=None):
+    #Scratch:
+    # tabulate_temps(w['main]['temp'])
+
+    # return '<p>The temperature is %s degrees Fahrenheit.</p>\n' % (w['main']['temp'])
+    print("Value of a : " + str(a))
+    print("Value of b : " + str(b))
+
+    return '<p>Test<p>'
 
 # looks for an 'application' callable by default
 application = Flask(__name__)
 
 # some bits of text for the page
-header_text = '''
+header_text = tabulate_temps(1,2) + '''
     <html>\n<head> <title>WeatherView</title> </head>\n<body>'''
 footer_text = '</body>\n</html>'
 
 # add a rule for the index page
-application.add_url_rule('/', 'index', (lambda: header_text + print_temp(requests.get(
-        'https://api.openweathermap.org/data/2.5/weather?lat=41.9&lon=-87.6&units=imperial&appid=75ee49c19c9fac22f81ffcac43b80552').json()) + footer_text))
+application.add_url_rule('/', 'index', (lambda: header_text))
+        #print_temp(requests.get(
+        # 'https://api.openweathermap.org/data/2.5/weather?lat=41.9&lon=-87.6&units=imperial&appid=75ee49c19c9fac22f81ffcac43b80552').json()) + footer_text))
 
 if __name__ == "__main__":
     # It is best to reset the below variable to false for a production deployment
