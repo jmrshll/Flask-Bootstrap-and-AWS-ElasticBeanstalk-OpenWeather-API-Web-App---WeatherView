4d2c1790a132c487ae3398994204c435046f1734
jmrshll <33238796+jmrshll@users.noreply.github.com>
2022-11-01 00:57:52 -0700

    reworked app into a basic HTML graphical compass

--- application.py
+++ application.py
@@ -25,8 +25,8 @@ LON = -87.6
 #         '&units=imperial&appid=75ee49c19c9fac22f81ffcac43b80552').json()
 
 # print the temperature
-def print_temp(w):
-    return '<p>The temperature is %s degrees Fahrenheit.' % (w['main']['temp']) + '</p>\n'
+# def print_temp(w):
+#     return '<p>The temperature is %s degrees Fahrenheit.' % (w['main']['temp']) + '</p>\n'
 
 
 # this method is for tabulating together and then averaging
@@ -36,37 +36,41 @@ def print_temp(w):
 # and longitudinal coordinates
 
 def tabulate_temps(a=None, b=None):
-    #Scratch:
-    # tabulate_temps(w['main]['temp'])
+    temp_center = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
 
     temp_kilometer_away_north = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT + 1) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
     temp_kilometer_away_south = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT - 1) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
     temp_kilometer_away_east = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON + 1) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
     temp_kilometer_away_west = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT - 1) + '&lon=' + str(LON - 1) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
 
-    readout_msg_1 = '<p>Latitude: ' + str(a) + '; Longitude: ' + str(b) + '<p>\n'
-    readout_msg_north = '<p>Temperature 1 degree north: ' + str(temp_kilometer_away_north) + '</p>\n'
-    readout_msg_south = '<p>Temperature 1 degree south: ' + str(temp_kilometer_away_south) + '</p>\n'
-    readout_msg_east = '<p>Temperature 1 degree east: ' + str(temp_kilometer_away_east) + '</p>\n'
-    readout_msg_west = '<p>Temperature 1 degree west: ' + str(temp_kilometer_away_west) + '</p>\n'
+    # a simple graphical compass display using a 3x3 HTML table
+    row_top = '<tr><td>\</td><td>%s</td><td>/</td>' % temp_kilometer_away_north
+    row_middle = '<tr><td>%s</td><td>%s</td><td>%s</td>' % (temp_kilometer_away_west, temp_center, temp_kilometer_away_east)
+    row_bottom = '<tr><td>/</td><td>%s</td><td>\</td>' % temp_kilometer_away_south
 
-    return readout_msg_1 + readout_msg_north + readout_msg_south + readout_msg_east + readout_msg_west
+    compass_table = '<table>' + row_top + row_middle + row_bottom + '</table>'
+
+    return compass_table
 
 # looks for an 'application' callable by default
 application = Flask(__name__)
 
 # some bits of text for the page
 header_text = tabulate_temps(LAT,LON) + '''
-    <html>\n<head> <title>WeatherView</title> </head>\n<body>'''
+    <html>
+        <head>
+            <link rel="stylesheet" href="static/styles.css">
+                <title>WeatherView</title>
+        </head>
+    <body>'''
 
 footer_text = '</body>\n</html>'
 
 # add a rule for the index page
-application.add_url_rule('/', 'index', (lambda: header_text +
-         print_temp(requests.get(
-         'https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()) + footer_text))
+application.add_url_rule('/', 'index', (lambda: header_text + footer_text))
 
 if __name__ == "__main__":
     # It is best to reset the below variable to false for a production deployment
    application.debug = True
-   application.run()
+   application.run(host="0.0.0.0", port=8080, threaded=True)
+   
