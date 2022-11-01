6f91300e94c51063a2feb735c1741c00932bd175
jmrshll <33238796+jmrshll@users.noreply.github.com>
2022-11-01 00:09:19 -0700

    tabulation of temperatures 1 degree away NSEW

--- application.py
+++ application.py
@@ -7,8 +7,10 @@ import requests
 
 import json
 
+OPENWEATHER_APP_ID = '75ee49c19c9fac22f81ffcac43b80552'
+
 LAT = 41.9
-LON = 87.6
+LON = -87.6
 
 # OpenWeatherMap API Key: 75ee49c19c9fac22f81ffcac43b80552
 
@@ -27,20 +29,28 @@ def print_temp(w):
     return '<p>The temperature is %s degrees Fahrenheit.' % (w['main']['temp']) + '</p>\n'
 
 
-# this method is for tabulating together the temperatures
-# a kilometer to the North, South, East, and West from
-# the input of the latitudinal and longitudinal coordinates
-#
-# tabulate_temps
+# this method is for tabulating together and then averaging
+# out the temperatures a single latitudinal or longitudinal
+# degree shift (equivalent to ~111 km) away to the North,
+# South, East, and West from the input of supplied latitudinal
+# and longitudinal coordinates
+
 def tabulate_temps(a=None, b=None):
     #Scratch:
     # tabulate_temps(w['main]['temp'])
 
-    # return '<p>The temperature is %s degrees Fahrenheit.</p>\n' % (w['main']['temp'])
-    print("Value of a : " + str(a))
-    print("Value of b : " + str(b))
+    temp_kilometer_away_north = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT + 1) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
+    temp_kilometer_away_south = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT - 1) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
+    temp_kilometer_away_east = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON + 1) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
+    temp_kilometer_away_west = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT - 1) + '&lon=' + str(LON - 1) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()['main']['temp']
 
-    return '<p>Latitude: ' + str(a) + '; Longitude: ' + str(b) + '<p>'
+    readout_msg_1 = '<p>Latitude: ' + str(a) + '; Longitude: ' + str(b) + '<p>\n'
+    readout_msg_north = '<p>Temperature 1 degree north: ' + str(temp_kilometer_away_north) + '</p>\n'
+    readout_msg_south = '<p>Temperature 1 degree south: ' + str(temp_kilometer_away_south) + '</p>\n'
+    readout_msg_east = '<p>Temperature 1 degree east: ' + str(temp_kilometer_away_east) + '</p>\n'
+    readout_msg_west = '<p>Temperature 1 degree west: ' + str(temp_kilometer_away_west) + '</p>\n'
+
+    return readout_msg_1 + readout_msg_north + readout_msg_south + readout_msg_east + readout_msg_west
 
 # looks for an 'application' callable by default
 application = Flask(__name__)
@@ -54,7 +64,7 @@ footer_text = '</body>\n</html>'
 # add a rule for the index page
 application.add_url_rule('/', 'index', (lambda: header_text +
          print_temp(requests.get(
-         'https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON) + '&units=imperial&appid=75ee49c19c9fac22f81ffcac43b80552').json()) + footer_text))
+         'https://api.openweathermap.org/data/2.5/weather?lat=' + str(LAT) + '&lon=' + str(LON) + '&units=imperial&' + 'appid=' + OPENWEATHER_APP_ID).json()) + footer_text))
 
 if __name__ == "__main__":
     # It is best to reset the below variable to false for a production deployment
